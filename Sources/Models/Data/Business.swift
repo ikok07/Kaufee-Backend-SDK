9a37604584298392e65c3bf0170c25f65ed7ea8a
ikok07 <kokmarok@gmail.com>
2024-01-28 15:42:52 +0200

    conform business to hashable

--- Sources/Models/Data/Business.swift
+++ Sources/Models/Data/Business.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct Business: Codable {
+public struct Business: Codable, Hashable {
     public let userId: String
     public let name: String
     public let description: String
@@ -15,7 +15,7 @@ public struct Business: Codable {
     public let metadata: BusinessMetadata
 }
 
-public struct BusinessProducts: Codable {
+public struct BusinessProducts: Codable, Hashable {
     public let businessId: String
     public let name: String
     public let description: String
@@ -24,6 +24,6 @@ public struct BusinessProducts: Codable {
     public let metadata: BusinessMetadata
 }
 
-public struct BusinessMetadata: Codable {
+public struct BusinessMetadata: Codable, Hashable {
     public let creation: Date
 }
