070b3cf4e88ff404792216a4521d44169698007b
ikok07 <kokmarok@gmail.com>
2024-01-30 20:21:20 +0200

    fix bug

--- Sources/Backend Methods/Business/Update/UpdateBusiness.swift
+++ Sources/Backend Methods/Business/Update/UpdateBusiness.swift
@@ -5,25 +5,26 @@
 //  Created by Kaloyan Petkov on 30.01.24.
 //
 
-import UIKit
+import SwiftUI
 import NetworkRequests
 import JSONCoder
 
 extension Backend {
     
-    public func updateBusiness(name: String, description: String, image: UIImage?, userId: String, businessId: String, token: String, callback: (Result<ModifyBusinessResponse, BackendError<String>>) async -> Void) async {
+    public func updateBusiness(name: String, description: String, image: Image?, userId: String, businessId: String, token: String, callback: (Result<ModifyBusinessResponse, BackendError<String>>) async -> Void) async {
         guard let config else {
             await callback(.failure(K.SDKError.noConfigError))
             return
         }
         
         let jsonData = JSONCoder.encode(ModifyBusinessRequest(name: name, description: description))
+        let uiImage = ImageRenderer(content: image).uiImage
         
         let request: Result<ModifyBusinessResponse, NetworkError> = await Request.formData(
             httpMethod: "PATCH",
             url: "\(config.baseUrl)/en/api/v1/user/\(userId)/business/\(businessId)",
             json: jsonData,
-            image: image,
+            image: uiImage,
             authToken: token
         )
         
